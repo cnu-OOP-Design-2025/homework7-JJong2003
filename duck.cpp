006d92a97ee2b179019019f7a8388279a18831b3
JonghHyeon <jonghp1128@naver.com>
2025-11-04 13:11:41 +0900

    [add] 클래스 생성자 내용 구현, Duck의 함수 구현

--- duck.cpp
+++ duck.cpp
@@ -1,22 +1,54 @@
 #include "duck.h"
 
 /* TODO */
-void Duck::performFly(){}
+void Duck::performFly(){
+    Duck::flyBehavior->fly();
+}
 /* TODO */
-void Duck::performQuack(){}
+void Duck::performQuack(){
+    Duck::quackBehavior->quack();
+}
 /* TODO */
-void Duck::setFlyBehavior(FlyBehavior* fb){}
+void Duck::setFlyBehavior(FlyBehavior* fb){
+    if (Duck::flyBehavior != nullptr) 
+        delete Duck::flyBehavior;
+    Duck::flyBehavior = fb;
+}
 /* TODO */
-void Duck::setQuackBehavior(QuackBehavior* qb){}
+void Duck::setQuackBehavior(QuackBehavior* qb){
+    if (Duck::quackBehavior != nullptr) 
+        delete Duck::quackBehavior;
+    Duck::quackBehavior = qb;
+}
 
 
 /* TODO */
-MallardDuck::MallardDuck() {}
+MallardDuck::MallardDuck() {
+    // 생성자에서 setting을 끝낸다.
+    MallardDuck::setFlyBehavior(new FlyWithWings);
+    MallardDuck::setQuackBehavior(new Quack);
+}
 /* TODO */
-RedheadDuck::RedheadDuck() {}
+RedheadDuck::RedheadDuck() {
+    RedheadDuck::setFlyBehavior(new FlyWithWings);
+    RedheadDuck::setQuackBehavior(new Quack);
+}
 /* TODO */
-RubberDuck::RubberDuck() {}
+RubberDuck::RubberDuck() {
+    RubberDuck::setFlyBehavior(new FlyNoWay);
+    RubberDuck::setQuackBehavior(new Squeak);
+}
 /* TODO */
-DecoyDuck::DecoyDuck() {}
+DecoyDuck::DecoyDuck() {
+    DecoyDuck::setFlyBehavior(new FlyNoWay);
+    DecoyDuck::setQuackBehavior(new MuteQuack);
+}
 /* TODO */
-ModelDuck::ModelDuck() {}
+ModelDuck::ModelDuck() {
+    ModelDuck::setFlyBehavior(new FlyNoWay);
+    ModelDuck::setQuackBehavior(new MuteQuack);
+    
+    // ModelDuck::flyBehavior->fly();
+    // ModelDuck::quackBehavior->quack();
+    // ModelDuck::display();
+}
